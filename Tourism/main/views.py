6c1e55d7fbae038d9aa8f025a3a0f7ea96b4044a
tcf12 <tcf123@mail.ru>
2026-03-02 14:20:51 +0700

    1.1.3. Шаблонизатор Jinja и HTML шаблоны

--- Tourism/main/views.py
+++ Tourism/main/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Методы для main.urls
 
 def index(request):
-    return HttpResponse("<h4>Hello World</h4>")
+    return render(request, 'main/index.html')
 
 def about(request):
-    return HttpResponse("<h4>Страница про нас</h4>")
+    return render(request, 'main/about.html')
