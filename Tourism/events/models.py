698d065db27fee2722b409ab6c90a4ff68da43c8
Tatiana Naidenova <tcf123@mail.ru>
2026-07-04 18:06:34 +0700

    2.10 Почти готов к релизу (поправить страницу Собери Тур на ПК версии)

--- Tourism/events/models.py
+++ Tourism/events/models.py
@@ -3,9 +3,13 @@ from django.db import models
 
 class Event(models.Model):
     title = models.CharField('Название события', max_length=200)
-    date = models.DateTimeField('Дата события')
+    date_start = models.DateField('Дата начала')
+    date_end = models.DateField('Дата окончания', blank=True, null=True)
     location = models.CharField('Место проведения', max_length=200)
-    description = models.TextField('Описание события')
+    description = models.TextField('Описание события', blank=True)
+
+    price = models.CharField('Вход', max_length=100, blank=True, default='Вход свободный')
+    source_link = models.URLField('Подробнее по ссылке', blank=True)
     image = models.ImageField('Фото события', upload_to='events/')
 
     def __str__(self):
